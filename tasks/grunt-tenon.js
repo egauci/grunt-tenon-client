d8f1d90cd9bf566d5e93798ff792d7cbc2846c61
unknown <egauci@DTC2100CC489BDA.ent.wfb.bank.corp>
2014-09-17 12:37:45 -0700

    Minor code cleanup - colorized some messages

--- tasks/grunt-tenon.js
+++ tasks/grunt-tenon.js
@@ -3,64 +3,70 @@
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('tenon', 'Grunt plugin for tenon', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var done = this.async(),
+    var merge = require('merge'),
+        chalk = require('chalk'),
+        tenon = require('../lib/tenon'),
+        done = this.async(),
         options = this.options({
           config: '.tenonrc'
         }),
         files = this.filesSrc,
-        tenon = require('../lib/tenon'),
-        merge = require('merge'),
         failed = 0,
         snippet = options.snippet,
         writePath = options.saveOutputIn,
-        allOut = {},
-        procFile
+        allOut = {}
     ;
 
-    delete options.snippet;
+    delete options.snippet; // remove grunt task options before passing to tenon module
     delete options.saveOutputIn;
 
-    procFile = function() {
+    function fmtFilename(file) {
+      var lix = file.lastIndexOf('/');
+      if (lix < 0) {
+        return  chalk.bold(file);
+      }
+      return file.slice(0, lix+1) + chalk.bold(file.slice(lix+1));
+    }
+
+    function procFile() { // process one html file
       var file = files.shift(),
           opts
       ;
-      opts = merge(options);
       if (!file) { // no more files, done.
         if (writePath && Object.keys(allOut).length > 0) {
           grunt.file.write(writePath, JSON.stringify(allOut, null, '  '));
         }
-        if (failed > 0) {
-          grunt.log.writeln('\nFiles with errors: ' + failed + '\n');
+        if (failed) {
+          grunt.log.writeln(chalk.yellow('\nFiles with errors: ' + failed + '\n'));
           done(false);
         } else {
           done(true);
         }
         return;
       }
+      opts = merge(options);
       opts.url = file;
       tenon(opts, function(err, res) {
-        grunt.log.write('\nFile: ' + file + ' ');
         if (err) {
-          grunt.log.writeln('');
-          grunt.log.error(err);
+          grunt.log.error('Tenon error: ' + err);
           done(false);
           return;
         }
+        grunt.log.write('\n' + fmtFilename(file) + ' ');
         if (res.resultSetFiltered.length > 0) {
           failed += 1;
           grunt.log.writeln('');
           res.resultSetFiltered.forEach(function(itm) {
-            grunt.log.error('  tID:   ' + itm.tID);
-            grunt.log.writeln('  Title: ' + itm.errorTitle);
-            grunt.log.writeln('  Xpath: ' + itm.xpath);
+            grunt.log.error(' tID:   ' + itm.tID);
+            grunt.log.writeln('   Title: ' + itm.errorTitle);
+            grunt.log.writeln('   Xpath: ' + itm.xpath);
             if (snippet) {
-              grunt.log.writeln('  Snippit:');
-              grunt.log.writeln(itm.errorSnippet.replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
+              grunt.log.writeln('   Snippit:');
+              grunt.log.writeln(chalk.gray(itm.errorSnippet.replace(/&lt;/g, '<').replace(/&gt;/g, '>')));
             }
           });
         } else {
-          grunt.log.ok(' OK');
+          grunt.log.ok(chalk.green('OK'));
         }
         if (writePath) {
           delete res.resultSetFiltered;
@@ -68,7 +74,7 @@ module.exports = function(grunt) {
         }
         process.nextTick(procFile);
       });
-    };
+    }
 
     procFile();
   });
